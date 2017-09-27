c1a241148ac148b828f86f8129a812a1611ac8dc
hugobullont <hugobullont@gmail.com>
2017-09-27 15:44:38 -0500

    Agregado Application

---
+++ app/src/main/java/me/sadboyz/freelo/models/Application.java
@@ -0,0 +1,13 @@
+package me.sadboyz.freelo.models;
+
+/**
+ * Created by Hugo on 27/09/2017.
+ */
+
+public class Application {
+    private String idApplication;
+    private String idWork;
+    private String idUser;
+    private String date;
+    private boolean status;
+}
