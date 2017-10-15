9c1868d31f6a27e6f4151330b4bda33e351e2d9f
Dypay <leohidalgo1997@gmail.com>
2017-10-15 02:04:09 -0500

    Versión 2.0 de Rewards (Avance)

--- app/src/main/java/me/sadboyz/freelo/activities/RewardsActivity.java
+++ app/src/main/java/me/sadboyz/freelo/activities/RewardsActivity.java
@@ -1,6 +1,7 @@
 package me.sadboyz.freelo.activities;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +23,9 @@ import com.bumptech.glide.RequestManager;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import me.sadboyz.freelo.R;
+import me.sadboyz.freelo.global.SessionVariables;
 import me.sadboyz.freelo.models.Reward;
+import me.sadboyz.freelo.repositories.ApplicationsRepository;
 import me.sadboyz.freelo.repositories.ImagesRepository;
 
 /**
@@ -31,9 +35,12 @@ import me.sadboyz.freelo.repositories.ImagesRepository;
 public class RewardsActivity extends AppCompatActivity {
     TextView nameTextView;
     TextView descriptionTextView;
-    TextView priceTextView;
-    TextView quantityTextView;
+    //TextView priceTextView;
+    //TextView quantityTextView;
     ImageView pictureImageView;
+    Button buttonR;
+
+    boolean apply = false;
 
 
     @Override
@@ -63,15 +70,14 @@ public class RewardsActivity extends AppCompatActivity {
     private void loadInfoRewards(final Reward reward) {
         nameTextView = (TextView)findViewById(R.id.rewardsNameTextView);
         descriptionTextView= (TextView)findViewById(R.id.rewardsDescriptionTextView);
-        priceTextView = (TextView)findViewById(R.id.rewardsPriceTextView);
-        quantityTextView = (TextView)findViewById(R.id.rewardsQuantityTextView);
+        //priceTextView = (TextView)findViewById(R.id.rewardsPriceTextView);
+        //quantityTextView = (TextView)findViewById(R.id.rewardsQuantityTextView);
         pictureImageView = (ImageView)findViewById(R.id.rewardsPictureView);
-
-
-        nameTextView.setText(reward.getName());
+        nameTextView.setText(reward.getName()+" a "+"S/"+String.format("%.2f",reward.getPrice()) );
         descriptionTextView.setText(reward.getDescription());
-        priceTextView.setText(String.format("%.2f",reward.getPrice()));
-        quantityTextView.setText(String.valueOf(reward.getQuantity()));
+        buttonR = (Button) findViewById(R.id.buttonR);
+        //priceTextView.setText(String.format("%.2f",reward.getPrice()));
+        //quantityTextView.setText(String.valueOf(reward.getQuantity()));
         //pictureImageView.setImageResource(reward.getPictureID());
 
 
@@ -84,7 +90,35 @@ public class RewardsActivity extends AppCompatActivity {
             }
         });
 
+        buttonR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showApplyAlert();
+            }
+        });
 
+        if(apply){
+            buttonR.setVisibility(View.INVISIBLE);
+        }
+
+
+    }
+
+    private void showApplyAlert() {
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(this, R.style.AppCompatAlertDialogStyle);
+        builder.setTitle("Canjear Cupon");
+        builder.setMessage("Su cupon de descuento se le  enviara a un lugar por definir \n\n" +
+                "¡Freelo informa... tal vez te lo enviemos por FB !");
+        builder.setPositiveButton("Aplicar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                apply = true;
+                buttonR.setVisibility(View.INVISIBLE);
+            }
+        });
+        builder.setNegativeButton("Cancelar", null);
+        builder.show();
     }
 
     public void loadImage(RequestManager glide, String url, ImageView view)
